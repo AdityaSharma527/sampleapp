b9355bfc4f46bfd9ac96a4773e5cf1f5c51771a2
AdityaSharma527 <aditya.sharma@leftrightmind.com>
2021-09-20 10:48:47 +0530

    dev1 changes sop

--- sample_app/src/main/java/sample_app/sop.java
+++ sample_app/src/main/java/sample_app/sop.java
@@ -5,6 +5,7 @@ public class sop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello from sts");
+System.out.println("developer 1 changes");
 	}
 
 }
